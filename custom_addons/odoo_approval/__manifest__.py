f28826a7196e286eb3782e8e532c27c74e1cb036
sree <sreejithoctec@gmail.com>
2026-02-14 02:06:38 +0100

    [custom-Task-005][IMP]odoo_approval: custom first level filter modify

--- custom_addons/odoo_approval/__manifest__.py
+++ custom_addons/odoo_approval/__manifest__.py
@@ -8,7 +8,7 @@
     "data": [
         "security/odoo_approval_security_groups.xml",
         "security/ir.model.access.csv",
-        #"views/purchase_order_approval_filter_view.xml",
+        "views/purchase_order_approval_filter_view.xml",
         "views/res_config_settings_approval_views.xml",
         "views/purchase_order_view.xml"
         
